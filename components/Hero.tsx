30c054b4fec3d6179383515f988293a6b5efe188
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-03-22 15:11:34 +0200

    Working on the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero'>
-      Hero
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book and rent a car -- quickly and easily!
+        </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking process.
+        </p>
+      </div>
     </div>
   )
 }
