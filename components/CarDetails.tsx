9bb156e509979f6513dc8636f9c749fef2ee0b64
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-03-30 22:20:50 +0200

    Done with CarDetails

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,7 +41,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
               leaveTo='opacity-0 scale-95'
               >
                 <Dialog.Panel 
-                  className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col"
+                  className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col"
                 >
                   <button
                     type='button'
@@ -105,6 +105,15 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <h2 className="font-semibold text-xl capitalize">
                       {car.make} {car.model}
                     </h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className="flex justify-between gap-5 w-full text-right" key={key}>
+                          <h4 className='text-grey capitalize'>{key.split("_").join(" ")}</h4>
+                          <p className='text-black-100 font-semibold'>{value}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>   
