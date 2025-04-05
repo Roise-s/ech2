350045976ed2b9317201ca2e853788f61031b19e
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-04-05 13:09:32 +0200

    3.0.1

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,19 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
             'x-rapidapi-key': '0cfae8fb0bmsh0d8e9767eed3e53p140655jsn7c5ba1379023',
             'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
-        headers: headers,
-    });
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${model}`,
+        {
+          headers: headers,
+        }
+      );
 
     const result = await response.json();
 
